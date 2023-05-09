37e4a76a1888915e6dafa1dcd50c120affe73ef0
Marcus Kolber <marcus.kolber@gmail.com>
2023-05-09 11:34:13 -0400

    Scratchpad MOD + '

--- config.def.h
+++ config.def.h
@@ -66,7 +66,7 @@ static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_',      togglescratch,  {.v = scratchpadcmd } },
+	{ MODKEY,                       XK_apostrophe,      togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
