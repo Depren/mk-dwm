60590e15c07612d09c1ab18d9a33bbb434cabcfb
Marcus Kolber <marcus.kolber@gmail.com>
2023-05-10 12:11:33 -0400

    Fixed fullscreen

--- dwm.c
+++ dwm.c
@@ -200,7 +200,6 @@ static void sendmon(Client *c, Monitor *m);
 static void setclientstate(Client *c, long state);
 static void setfocus(Client *c);
 static void setfullscreen(Client *c, int fullscreen);
-static void fullscreen(const Arg *arg);
 static void setsticky(Client *c, int sticky);
 static void setlayout(const Arg *arg);
 static void setmfact(const Arg *arg);
