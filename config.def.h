6b471516d5cdcf4d57cb6f97f64a956a6f0c253b
Marcus Kolber <marcus.kolber@gmail.com>
2023-05-11 11:00:08 -0400

    Fibonacci and bottom stack implementation

--- config.def.h
+++ config.def.h
@@ -42,6 +42,10 @@ static const Layout layouts[] = {
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "TTT",      bstack },  /* bottomstack */
+	{ "===",      bstackhoriz }, /* bottomstack horizontal */
+    { "[@]",      spiral }, /* fibonacci - spiral */
+    { "[\\]",     dwindle }, /* fibonacci - dwindle */
 };
 
 /* key definitions */
@@ -77,6 +81,11 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[4]} },
+    { MODKEY,                       XK_u,      setlayout,      {.v = &layouts[5]} },
+	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[6]} },
+
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
