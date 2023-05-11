33ffdaf5b267d69e184a6bc5e9d820841b5fdeb7
Marcus Kolber <marcus.kolber@gmail.com>
2023-05-11 12:45:55 -0400

    Increased border pixel size

--- config.def.h
+++ config.def.h
@@ -7,7 +7,7 @@
 #include "fibonacci.c"
 
 /* appearance */
-static unsigned int borderpx  = 1;        /* border pixel of windows */
+static unsigned int borderpx  = 3;        /* border pixel of windows */
 static unsigned int snap      = 32;       /* snap pixel */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
